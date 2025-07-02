70c01f037e6a09a11d22859ecaaff3f9331ce9fb
Vic <159442729+V1ctorMH@users.noreply.github.com>
2025-07-02 20:57:20 -0300

    Novas rotas criadas no App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Registroscreen from './screens/Registroscreen';
 import Loginscreen from './screens/Loginscreen';
 import Testescreen from './screens/Testescreen';
-import Homescreen from './screens/Homescreen'; 
+import Homescreen from './screens/Homescreen';
+import RegistroNoticiascreen from './screens/RegistroNoticiascreen'; 
 
 const Stack = createStackNavigator();
 
@@ -16,7 +18,8 @@ export default function App() {
         <Stack.Screen name="Registro" component={Registroscreen} />
         <Stack.Screen name="Login" component={Loginscreen} />
         <Stack.Screen name="Testescreen" component={Testescreen} />
-        <Stack.Screen name="Home"component={Homescreen}/>
+        <Stack.Screen name="Home"component={Homescreen} />
+        <Stack.Screen name="RegistroNoticiascreen"component={RegistroNoticiascreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
